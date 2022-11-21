4324ebabc5e0c1dbd53d456fe2b013ce324d6f7b
Toggy Smith <tony.o.smith@kcl.ac.uk>
2022-11-21 21:22:52 +0000

    Revert "feat: create User model"
    This reverts commit c8193708cfcb3fb1ed1acc762bb7c0da872dd089.

--- lessons/models.py
+++ lessons/models.py
@@ -1,9 +1,3 @@
 from django.db import models
 
 # Create your models here.
-from django.db import models
-from django.contrib.auth.models import AbstractUser
-
-
-class User(AbstractUser):
-    email = models.EmailField(unique=True)
