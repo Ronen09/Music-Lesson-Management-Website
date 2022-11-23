d77707e5398934c83f7cc9f5687d20588f29cca8
Toggy Smith <tony.o.smith@kcl.ac.uk>
2022-11-23 12:31:59 +0000

    feat: add User foreign key to LessonRequest model

--- lessons/models/lesson_request.py
+++ lessons/models/lesson_request.py
@@ -1,5 +1,7 @@
 from django.db import models
-from django.core.validators import MaxValueValidator, MinValueValidator
+from django.core.validators import MinValueValidator
+
+from lessons.models import User
 
 
 class LessonRequest(models.Model):
@@ -14,3 +16,5 @@ class LessonRequest(models.Model):
     lesson_duration_in_mins = models.IntegerField(validators=[MinValueValidator(1)])
 
     further_information = models.CharField(max_length=255, null=True, blank=True)
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
