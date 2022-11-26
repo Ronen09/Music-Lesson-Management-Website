a989cac8d478134762add478b08ef7a7cb083ac2
Toggy Smith <tony.o.smith@kcl.ac.uk>
2022-11-26 00:31:44 +0000

    fix: register Term model in admin.py

--- lessons/admin.py
+++ lessons/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from lessons.models import User, LessonRequest
+from lessons.models import User, LessonRequest, Term
 
 # Register your models here.
 admin.site.register(User)
 admin.site.register(LessonRequest)
+admin.site.register(Term)
