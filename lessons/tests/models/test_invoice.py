d2829dd4ed31b8ef5821dcd6ccba6f173462d97f
Toggy Smith <tony.o.smith@kcl.ac.uk>
2022-12-03 21:26:51 +0000

    test: add tests for Invoice model

---
+++ lessons/tests/models/test_invoice.py
@@ -0,0 +1,32 @@
+from django.core.exceptions import ValidationError
+from django.test import TestCase
+
+from lessons.models import Invoice
+
+
+class TestInvoice(TestCase):
+    fixtures = ["lessons/tests/fixtures/invoice-fixture.json"]
+
+    def setUp(self):
+        self.invoice = Invoice.objects.get(pk=5)
+
+    def test_valid_invoice(self):
+        self._assert_invoice_is_valid()
+
+    def test_lesson_request_cannot_be_none(self):
+        self.invoice.lesson_request = None
+        self._assert_invoice_is_invalid()
+
+    def test_user_cannot_be_none(self):
+        self.invoice.user = None
+        self._assert_invoice_is_invalid()
+
+    def _assert_invoice_is_valid(self):
+        try:
+            self.invoice.full_clean()
+        except ValidationError:
+            self.fail("Test invoice should be valid.")
+
+    def _assert_invoice_is_invalid(self):
+        with self.assertRaises(ValidationError):
+            self.invoice.full_clean()
