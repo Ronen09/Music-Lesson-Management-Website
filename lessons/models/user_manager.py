1e77c0688e0167356b84d1169ede816a9cce6bf0
Toggy Smith <tony.o.smith@kcl.ac.uk>
2022-12-05 22:19:51 +0000

    chore: redo the seed command to fulfill requirements

--- lessons/models/user_manager.py
+++ lessons/models/user_manager.py
@@ -2,6 +2,7 @@ from django.contrib.auth.base_user import BaseUserManager
 
 
 class UserManager(BaseUserManager):
+
     def create_user(self, email, password, **extra_fields):
         if not email:
             raise ValueError("An email must be provided.")
@@ -24,4 +25,4 @@ class UserManager(BaseUserManager):
         if extra_fields.get("is_staff") is False:
             raise ValueError("A superuser must have `is_staff = True`.")
 
-        return self.create_user(email, password, **extra_fields)
+        return self.create_user(email, password, **extra_fields, role="Superuser")
