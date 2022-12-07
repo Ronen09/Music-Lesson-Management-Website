67286637c267eca167b29fc8d27ec209319cea5f
Toggy Smith <tony.o.smith@kcl.ac.uk>
2022-12-07 21:42:52 +0000

    refactor: make seed command automatically get models to delete

--- lessons/management/commands/unseed.py
+++ lessons/management/commands/unseed.py
@@ -1,3 +1,4 @@
+from django.apps import apps
 from django.core.management.base import BaseCommand, CommandError
 
 from lessons.models import (Invoice, Lesson, LessonRequest, Payment, Teacher, Term, User)
@@ -9,13 +10,13 @@ class Command(BaseCommand):
         print("Unseeding initial data...")
 
         try:
-            User.objects.filter(is_superuser=False).delete()
-            Term.objects.all().delete()
-            LessonRequest.objects.all().delete()
-            Invoice.objects.all().delete()
-            Teacher.objects.all().delete()
-            Lesson.objects.all().delete()
-            Payment.objects.all().delete()
+            app_models = apps.get_app_config("lessons").get_models()
+
+            for model in app_models:
+                if model.__name__ == "User":
+                    User.objects.filter(is_superuser=False).delete()
+                else:
+                    model.objects.all().delete()
         except:
             raise CommandError("Unable to unseed initial data.")
 
