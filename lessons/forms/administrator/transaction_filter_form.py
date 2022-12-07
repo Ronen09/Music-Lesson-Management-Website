72d88278de6891ec0fefe11f80728933ef04db12
Toggy Smith <tony.o.smith@kcl.ac.uk>
2022-12-07 20:55:29 +0000

    feat: show balances, invoices and payments for users to administrators

--- lessons/forms/administrator/transaction_filter_form.py
+++ lessons/forms/administrator/transaction_filter_form.py
@@ -4,8 +4,12 @@ from lessons.models import User
 
 
 class TransactionFilterForm(forms.Form):
-    """A form used to filter transactions by a single selected student."""
+    """A form used to filter transactions by a single selected student and transaction type (balance, invoice or payment)."""
+    transaction_choices = [("balances", "Balances"), ("invoices", "Invoices"), ("payments", "Payments")]
+
     student_filter = forms.ModelChoiceField(label="Filter by student:",
                                             queryset=User.objects.filter(role="Student", is_superuser=False),
                                             empty_label="All",
                                             required=False)
+
+    transaction_filter = forms.CharField(label="Filter by type:", widget=forms.Select(choices=transaction_choices))
