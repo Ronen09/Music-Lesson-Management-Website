9b8636497cbd4535473e92e4aa9364ac19949113
kabir <100319900+kabir505@users.noreply.github.com>
2022-12-08 11:37:29 +0000

    Update invoice.py

--- lessons/models/invoice.py
+++ lessons/models/invoice.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-
+#Create Invoices, for the user and for which lesson
 class Invoice(models.Model):
     lesson_request = models.ForeignKey("lessons.LessonRequest", on_delete=models.CASCADE)
     user = models.ForeignKey("lessons.User", on_delete=models.CASCADE)
